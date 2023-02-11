b5952b2d09c86360947acdb824b0946c3327a36b
Karan <19ucc025@lnmiit.ac.in>
2023-02-11 22:00:39 +0530

    first commit

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -11,20 +11,18 @@ interface Props{
     item: CartItemProps;
 }
 export default function CartItem({item, Data}: Props) {
-    const cart = Data.map(value=>{
-        if(value?.id?.includes(item.id))
-            console.log("YYAYAYAYAY")
-    })
+    const cart = Data.find((i)=>i.id===item.id)
+    console.log(cart);
     console.log(item.id);
-    console.log(Data);
+    //console.log(Data);
     if(cart==null){
         console.log("NULLLLLLLLLL")
         return null;
     }
   return (
     <div className='flex-row justify-between align-middle m-auto'>
-        {/* <div>{cart.name}</div>
-        <div>{cart.price}</div> */}
+        <div>{cart.name}</div>
+        <div>{cart.price}</div>
     </div>
   )
 }
