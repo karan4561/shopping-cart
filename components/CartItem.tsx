02be2c36008a0ed5c9dff351cccfa97de3350e32
Karan <19ucc025@lnmiit.ac.in>
2023-02-13 12:37:44 +0530

    Changed Made

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -7,22 +7,31 @@ type CartItemProps = {
 }
 
 interface Props{
-    Data: Menu[];
+    Data: any;
     item: CartItemProps;
 }
 export default function CartItem({item, Data}: Props) {
-    const cart = Data.find((i)=>i.id===item.id)
-    console.log(cart);
-    console.log(item.id);
+    //Data.map(i=>(console.log(i.id)));
+   // const cart = Object.values Data.find((i)=>i.id===item.id)
+
+   const x:any = [];
+   Data.map((D:any)=>D.map((D1:any)=>x.push(D1))) 
+   
+   const cart = Object.values(x).find((i:any)=>i.id===item.id);
+   console.log(x);
+    //console.log(cart);
+    //console.log(item.id);
     //console.log(Data);
     if(cart==null){
         console.log("NULLLLLLLLLL")
         return null;
     }
   return (
-    <div className='flex-row justify-between align-middle m-auto'>
-        <div>{cart.name}</div>
-        <div>{cart.price}</div>
+    <div className='flex-row description space-x-10 text-xl bg-slate-500 space-y-5 justify-between border m-auto'>
+        <div className='pl-2' >{cart.name}</div>
+        <div className='pb-4'>{cart.price}</div>
+        <div className='pb-4'>x{item.quantity}</div>
+        <div className='font-bold underline border-l pl-3 pb-4 pr-3'>Rs. {item.quantity*cart.price}</div>
     </div>
   )
 }
--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -9,16 +9,27 @@ interface Props{
 
 function Checkout({Data}: Props) {
     const {cartQuantity, cartItems} = useShoppingCart();
+    const x:any = [];
+    Data.map((D:any)=>D.map((D1:any)=>x.push(D1))) 
   return (
     <div>
         {cartQuantity>0 && (
             <div className='flex-col space-x-2'>
-                <p className='text-5xl m-auto'>Item Added to cart</p>   
+                <p className='items-center pb-6 bg pl-4 text-lg font-serif pt-72 m-auto'>My Bag :</p>   
                 {cartItems.map(item=>(
                     <CartItem 
                     item={item}
                     Data={Data}/>
                 ))}
+
+                <div className='pt-16 text-2xl font-extrabold underline font-mono'>Grand Total: Rs.{
+                    cartItems.reduce((total, cartItem) => {
+                        const cart = Object.values(x).find((i:any)=>i.id===cartItem.id)
+                        return total + (cart?.price||0)*cartItem.quantity},0)
+                    }
+                </div>
+
+                <button className='px-4 py-2 bg-yellow-100 text-black rounded-lg items-center mt-10 ml-18'>Proceed To Checkout</button>
             </div>
         )}
     </div>
