2740f1859dfa6816611144ccc175cc2183a07c26
Karan <19ucc025@lnmiit.ac.in>
2023-02-15 11:47:12 +0530

    Checkout Page

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,6 +1,6 @@
-const BASE_URL = 'https://free-food-menus-api-production.up.railway.app'
+export const BASE_URL = 'https://free-food-menus-api-production.up.railway.app'
 
-const requests = {
+export const requests = {
     fetchBurger: `${BASE_URL}/burgers`,
     fetchBBQs: `${BASE_URL}/bbqs`,
     fetchBreads: `${BASE_URL}/breads`,
